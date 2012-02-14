2c79b608e329d4a31b0f57343c6ab5bcc1deae97
Mike Plavsky <Mike.Plavsky@quest.com>
2012-02-14 16:29:23 +0000

    rules for default sg

--- creator.py
+++ creator.py
@@ -5,14 +5,15 @@ from boto import config as cfg
 
 conn = boto.connect_vpc()
 
-cidr = boto.config.get('VPC', 'cidr')
-dhcp = boto.config.get('VPC', 'dhcp-id') 
+cidr = cfg.get('VPC', 'cidr')
+dhcp = cfg.get('VPC', 'dhcp-id')
+ip = cfg.get('VPC', 'external-cidr')
 
 vpc = conn.create_vpc(cidr) 
 conn.create_subnet( vpc.id,cidr)
 
 group = [gr for gr in conn.get_all_security_groups() if gr.vpc_id == vpc.id][0] 
-print group.id
+conn.authorize_security_group(group_id=group.id, ip_protocol='tcp', from_port=0, to_port=65535, cidr_ip=ip)
 
 gate = conn.create_internet_gateway()
 conn.attach_internet_gateway( gate.id, vpc.id)
