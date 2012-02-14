5ade2e2726b472053ddef1e114e94710f2d16d80
Ubuntu <ubuntu@domU-12-31-38-04-96-F1.compute-1.internal>
2012-02-14 15:29:55 +0000

    Added security group

--- creator.py
+++ creator.py
@@ -4,12 +4,17 @@ import boto
 from boto import config as cfg
 
 conn = boto.connect_vpc()
-cidr = "10.0.0.0/24"
+
+cidr = boto.config.get('VPC', 'cidr')
+dhcp = boto.config.get('VPC', 'dhcp-id') 
 
 vpc = conn.create_vpc(cidr) 
 conn.create_subnet( vpc.id,cidr)
 
+group = conn.create_security_group("VPCIN", "important text", vpc.id)
+print group.id
+
 gate = conn.create_internet_gateway()
 conn.attach_internet_gateway( gate.id, vpc.id)
 
-conn.associate_dhcp_options( "dopt-c2ad5eaa", vpc.id )
+conn.associate_dhcp_options(dhcp , vpc.id)
